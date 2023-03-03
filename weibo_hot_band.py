6cc9c42a5690101aa070643bc648c2df42099b8b
hebe-tian <59718890+hebe-tian@users.noreply.github.com>
2023-03-04 02:57:45 +0800

    Add files via upload

---
+++ weibo_hot_band.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+"""
+@Author : hebe_tian
+"""
+import requests
+import json
+import jsonpath
+import logging
+
+
+class WeiboApi:
+    def __init__(self):
+        self.url = 'https://weibo.com'
+
+    def weibo_hot_band(self):
+        path = '/ajax/statuses/hot_band'
+        hot_band_response = requests.get(self.url + path)
+        hot_band_response = json.loads(hot_band_response.text)
+        band_list = {}
+        try:
+            note_list = jsonpath.jsonpath(hot_band_response, '$.data.band_list[*].note')
+            num_list = jsonpath.jsonpath(hot_band_response, '$.data.band_list[*].num')
+            for i in range(len(note_list)):
+                band_list[note_list[i]] = num_list[i]
+            return band_list
+
+        except Exception as e:
+            return logging.exception(e)
