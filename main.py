7925d0decfe2dc8d19da1c4976490c486d8a8bed
hebe_tien <linleil@126.com>
2023-03-28 15:35:03 +0800

    不需要WeiboAPi类，直接使用def定义方法，在main函数中调用

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import logging
-
 import weibo_hot_band
 
 
-hot_band_content = weibo_hot_band.WeiboApi()
-hot_band = hot_band_content.weibo_hot_band()
+hot_band = weibo_hot_band.weibo_hot_band()
 print(len(hot_band))
 print(hot_band)
 
-topic = hot_band_content.weibo_topic_band()
+topic = weibo_hot_band.weibo_topic_band()
 print(len(topic))
 print(topic)
 
--- weibo_hot_band.py
+++ weibo_hot_band.py
@@ -6,55 +6,54 @@ import requests
 import json
 import jsonpath
 import logging
-import time
 
 
-class WeiboApi:
-    def __init__(self):
-        self.url = 'https://weibo.com'
+url = 'https://weibo.com'
 
-    def weibo_hot_band(self):
-        path = '/ajax/statuses/hot_band'
-        hot_band_response = requests.get(self.url + path)
-        hot_band_response = json.loads(hot_band_response.text)
-        band_list = {}
+
+def weibo_hot_band():
+    path = '/ajax/statuses/hot_band'
+    hot_band_response = requests.get(url + path)
+    hot_band_response = json.loads(hot_band_response.text)
+    band_list = {}
+    try:
+        # gov hot band->name and gov_tag
+        gov_note = jsonpath.jsonpath(hot_band_response, '$.data.hotgov.note')
+        # gov_num = jsonpath.jsonpath(hot_band_response, '$.data.hotgov.num')
+        band_list[gov_note[0]] = 'gov'
+        # social hot band->name and hot_num
+        note_list = jsonpath.jsonpath(hot_band_response, '$.data.band_list[*].note')
+        num_list = jsonpath.jsonpath(hot_band_response, '$.data.band_list[*].num')
+        for i in range(len(note_list)):
+            band_list[note_list[i]] = num_list[i]
+        return band_list
+
+    except Exception as e:
+        return logging.exception(e)
+
+
+def weibo_topic_band():
+    path = '/ajax/statuses/topic_band'
+    params = {
+        'sid': 'v_weibopro',
+        'category': 'all'
+    }
+    hot_topic = {}
+    for page in (1, 2):
+        params['page'] = page
+
         try:
-            # gov hot band->name and gov_tag
-            gov_note = jsonpath.jsonpath(hot_band_response, '$.data.hotgov.note')
-            # gov_num = jsonpath.jsonpath(hot_band_response, '$.data.hotgov.num')
-            band_list[gov_note[0]] = 'gov'
-            # social hot band->name and hot_num
-            note_list = jsonpath.jsonpath(hot_band_response, '$.data.band_list[*].note')
-            num_list = jsonpath.jsonpath(hot_band_response, '$.data.band_list[*].num')
-            for i in range(len(note_list)):
-                band_list[note_list[i]] = num_list[i]
-            return band_list
+            topic_response = requests.get(url + path, params=params)
+            topic_response = json.loads(topic_response.text)
+            topic_list = jsonpath.jsonpath(topic_response, '$.data.statuses[*].topic')
+            summary_list = jsonpath.jsonpath(topic_response, '$.data.statuses[*].summary')
+
+            for i in range(len(topic_list)):
+                if summary_list[i] == '':
+                    summary_list[i] = False
+                hot_topic[topic_list[i]] = summary_list[i]
 
         except Exception as e:
             return logging.exception(e)
 
-    def weibo_topic_band(self):
-        path = '/ajax/statuses/topic_band'
-        params = {
-            'sid': 'v_weibopro',
-            'category': 'all'
-        }
-        hot_topic = {}
-        for page in (1, 2):
-            params['page'] = page
-
-            try:
-                topic_response = requests.get(self.url + path, params=params)
-                topic_response = json.loads(topic_response.text)
-                topic_list = jsonpath.jsonpath(topic_response, '$.data.statuses[*].topic')
-                summary_list = jsonpath.jsonpath(topic_response, '$.data.statuses[*].summary')
-
-                for i in range(len(topic_list)):
-                    if summary_list[i] == '':
-                        summary_list[i] = False
-                    hot_topic[topic_list[i]] = summary_list[i]
-
-            except Exception as e:
-                return logging.exception(e)
-
-        return hot_topic
+    return hot_topic
