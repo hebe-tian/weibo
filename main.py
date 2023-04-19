ff0a33c1a8eb577392b10dfe6fcb0f01c27d997e
hebe_tien <linleil@126.com>
2023-04-19 21:15:26 +0800

    get shanghai time

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import weibo_hot_band
+import datetime
+import pytz
 
+
+dt = datetime.datetime.now()
+print(dt.astimezone(tz=pytz.timezone('Asia/Shanghai')))
 
 hot_band = weibo_hot_band.weibo_hot_band()
 print(len(hot_band))
