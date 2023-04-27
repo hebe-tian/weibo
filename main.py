b7f7b24c014c456b8af45934280ac065afd3cf00
hebe_tien <linleil@126.com>
2023-04-27 10:24:21 +0800

    delete log length

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ dt = datetime.datetime.now()
 print(dt.astimezone(tz=pytz.timezone('Asia/Shanghai')))
 
 hot_band = weibo_hot_band.weibo_hot_band()
-print(len(hot_band))
 print(hot_band)
 
 topic = weibo_hot_band.weibo_topic_band()
-print(len(topic))
 print(topic)
 
