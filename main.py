97c0113f0ea2b99ba2aac28004f324d8bbbe70a3
hebe-tian <59718890+hebe-tian@users.noreply.github.com>
2023-08-02 15:55:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ print(dt.astimezone(tz=pytz.timezone('Asia/Shanghai')))
 hot_band = weibo_hot_band.weibo_hot_band()
 print(hot_band)
 
-topic = weibo_hot_band.weibo_topic_band()
-print(topic)
+# topic = weibo_hot_band.weibo_topic_band()
+# print(topic)
 
